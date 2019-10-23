5935289844d9ae4149430a7fc6eb1164b846826f
waspryo <wasp2123321@gmail.com>
2019-10-23 17:44:05 +0900

    set up

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pf = require('html-pdf');
+const cors = require('cors');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
