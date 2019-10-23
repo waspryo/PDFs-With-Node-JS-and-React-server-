46abfb63b976c4406d4e681ea62ae1c82116d344
waspryo <wasp2123321@gmail.com>
2019-10-23 18:19:19 +0900

    done coding

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 const pdf = require('html-pdf');
 const cors = require('cors');
 
-const pdfTemplate = require('./documents')
+const pdfTemplate = require('./documents');
+
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -12,19 +13,18 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// POST - PDF generation and fetching of the data
 app.post('/create-pdf', (req, res) => {
-    pdf.create(pdfTemplate(req.data), {}).toFile('result.pdf', (err) => {
-        if (err) {
-            return Promise.reject();
+    pdf.create(pdfTemplate(req.body), {}).toFile('result.pdf', (err) => {
+        if(err) {
+            res.send(Promise.reject());
         }
-        return Promise.resolve();
-    })
-})
 
-// GET - Sned the generated PDF to the client
+        res.send(Promise.resolve());
+    });
+});
+
 app.get('/fetch-pdf', (req, res) => {
-    res.sendFile(`$(__dirname)/result.pdf`)
+    res.sendFile(`${__dirname}/result.pdf`)
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
